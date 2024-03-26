3f5a1eaecede7f47fa62aa709ae62c30441b3205
yojicjung <dywlr74@naver.com>
2024-03-26 21:20:14 +0900

    app-service의 pojo 객체 primary, qualifier 진행중

--- user-interface/rest-api/src/main/kotlin/com/hexagonal/restapi/member/controller/MemberController.kt
+++ user-interface/rest-api/src/main/kotlin/com/hexagonal/restapi/member/controller/MemberController.kt
@@ -1,7 +1,6 @@
 package com.hexagonal.restapi.member.controller
 
 import com.hexagonal.appservice.test.MyCustomInterface
-import org.springframework.beans.factory.config.BeanPostProcessor
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RestController
 
